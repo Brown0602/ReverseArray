4b710af328aaf39f54d700b4c3eeaa2e393bef0f
Vladislav <117478509+Brown0602@users.noreply.github.com>
2023-04-07 18:34:43 +0300

    Add files via upload

---
+++ ArrayReverse.java
@@ -0,0 +1,11 @@
+package first;
+
+public class ArrayReverse {
+    public static void main(String[] args){
+        //Чтение исходных данных
+        int[] array = {1, 2, 3, 4, 5};
+        //Обработка и вывод исходных данных
+
+
+    }
+}
